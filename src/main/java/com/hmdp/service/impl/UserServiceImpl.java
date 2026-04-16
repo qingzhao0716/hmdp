364384958f9903d2f59d3983cd41855f9cd38922
zyp <3195238440@qq.com>
2026-04-16 22:18:43 +0800

    简单实现：登录

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -11,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -43,5 +46,50 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
 
         // 6.返回 ok
         return Result.ok();
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        // 1.校验手机号
+        String phone = loginForm.getPhone();
+        if(RegexUtils.isPhoneInvalid(phone)){
+            // 如果不符合，返回错误信息
+            return Result.fail("手机号格式错误！");
+        }
+
+        // 2.校验验证码
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+
+        // 3.没发送验证码或与用户填写的不一致，报错
+        if(cacheCode == null || !cacheCode.toString().equals(code)){
+            return Result.fail("验证码错误");
+        }
+
+        // 4.一致，根据手机号查询用户
+        User user = query().eq("phone", phone).one();
+
+        // 5.判断用户是否存在
+        if(user == null){
+            // 6.不存在，创建新用户并保存
+            user = createUserWithPhone(phone);
+        }
+
+        // 7.保存用户信息到 session中
+        session.setAttribute("user",user);
+
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        // 1.创建用户
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomNumbers(10));
+
+        // 2.保存用户
+        save(user);
+
+        return user;
     }
 }
