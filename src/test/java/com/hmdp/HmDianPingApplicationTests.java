27663368b2243b70bde51c44e0c2f7b05906cd3b
zyp <3195238440@qq.com>
2026-04-26 09:36:58 +0800

    发布并查看探店笔记

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -74,8 +74,8 @@ class HmDianPingApplicationTests {
 
     @Test
     void testSaveShop () throws InterruptedException {
-        Shop shop = shopService.getById(1L);
-        cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
+        Shop shop = shopService.getById(4L);
+        cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 4L, shop, 10L, TimeUnit.SECONDS);
     }
 
     /**
